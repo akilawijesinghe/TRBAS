a2c5da9738201c57dfcc2f74fc3e6241a0e0e90e
akilawijesinghe <akilawijesinghe94@gmail.com>
2024-09-13 11:06:09 +1000

    FIX :: Report module bugs

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -9,7 +9,11 @@ class Api_model extends CI_Model
     {
         $this->db->select('tbl_booking.*, tbl_advertisements.*');
         $this->db->from('tbl_booking');
-        $this->db->join('tbl_advertisements', 'tbl_advertisements.booking_id = tbl_booking.id', 'left');
+        $this->db->join('tbl_advertisements', 'tbl_advertisements.booking_id = tbl_booking.id', 'INNER');
+        $this->db->join('tbl_customer_details', 'tbl_customer_details.id = tbl_booking.customer_id', 'INNER');
+        $this->db->join('tbl_users', 'tbl_users.id = tbl_customer_details.user_id', 'INNER');
+        $this->db->where('tbl_users.active', 1);
+        $this->db->where('tbl_users.deleted_at', NULL);
         $this->db->where('tbl_booking.billboard_id', $billboard_id);
         $this->db->where('tbl_booking.active', 1);
         $this->db->where('tbl_advertisements.status', 1);
@@ -36,6 +40,8 @@ class Api_model extends CI_Model
         $this->db->select('*');
         $this->db->from('tbl_billboards');
         $this->db->where('mac_address', $mac_address);
+        $this->db->where('active', 1);
+        $this->db->where('deleted_at', NULL);
         $query = $this->db->get();
         return $query->row();
     }
